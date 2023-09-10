95961b3f344370a99053090c8553e184a4b37841
ZorAnderius <gabriel.zorander@gmail.com>
2023-09-10 13:01:08 +0100

    deleted temp function getArgumData

--- utils/index.js
+++ utils/index.js
@@ -1,11 +1,9 @@
 const createNewContact = require("./createNewContact");
-const getArgumData = require("./getArgumData");
 const readFromFile = require("./readFromFile");
 const writeToFile = require("./writeToFile");
 
 module.exports = {
   createNewContact,
-  getArgumData,
   readFromFile,
   writeToFile,
 };
